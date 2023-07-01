1bee8cd8e38072cd12e6e2e01deb2ac80b0c1b22
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2023-07-01 21:57:50 +0200

    gg

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -3,13 +3,19 @@ $(function () {
     $('.hamburger-menu-drop-down').slideUp(0);
     $('.fa-bars').on('click', function () {
         $('.hamburger-menu-drop-down').slideToggle(100);
+        if (screen.width < 540) {
+            $('.search-bar-header').slideUp(0);
+        }
         $('#checked-bar').slideUp(0);
         $('#add-modal').slideUp(0);
-        $('.search-bar-header').slideUp(0);
+        
     })
 
     //searchbar
-    $('.search-bar-header').slideUp(0);
+    if (screen.width < 540) {
+        $('.search-bar-header').slideUp(0);
+    }
+    
     $('.fa-magnifying-glass').on('click', function () {
         $('.search-bar-header').slideToggle(100);
         $('.hamburger-menu-drop-down').slideUp(0);
@@ -18,10 +24,14 @@ $(function () {
     });
 
     $('.fa-xmark').on('click', function () {
-        $('#inputQuery').val('');
+        if (screen.width < 540) {
+            $('.search-bar-header').slideUp(0);
+            $('#inputQuery').val('');
+        } else {
+            $('#inputQuery').val('');
+            $('.Results').slideUp(0);
+        }
     })
-
-    $('#SearchResults').load("/Customer/home/SearchResults/")
 
     $('.search-bar-header').on('keyup', function () {
         var input = $('#inputQuery').val();
@@ -34,7 +44,10 @@ $(function () {
     $('#add-modal').load("/Customer/home/AddModal");
     $('.fa-plus').on('click', function () {
         $('#add-modal').slideToggle(100);
-        $('.search-bar-header').slideUp(0);
+        if (screen.width < 540) {
+            $('.search-bar-header').slideUp(0);
+        }
+        
         $('#checked-bar').slideUp(0);
         $('.hamburger-menu-drop-down').slideUp(0);
     })
@@ -44,7 +57,9 @@ $(function () {
     $('#checked-bar').load("/Customer/home/Checked");
     $('.fa-check').on('click', function () {
         $('#checked-bar').slideToggle(100);
-        $('.search-bar-header').slideUp(0);
+        if (screen.width < 540) {
+            $('.search-bar-header').slideUp(0);
+        }
         $('#add-modal').slideUp(0);
         $('.hamburger-menu-drop-down').slideUp(0);
     })
